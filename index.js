ffd48bd1213922207fd5ac2f8d5fe08b9a5aec08
Thomas Schuster <thomasdahlind@gmail.com>
2022-10-06 19:49:16 -0300

    Testes de async.

---
+++ index.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function getError(e) {
+    throw new Error(chalk.red('Ocorreu um erro: ', e.code));
+}
+
+// async/await
+async function getFiles(p) {
+    try {
+        const content = await fs.promises.readFile(p, 'utf-8')
+        console.log(content);
+    } catch (error) {
+        getError(error)
+    }
+}
+
+// promises com then()
+// function getFiles(p) {
+//     fs.promises
+//         .readFile(p, 'utf-8')
+//         .then((content => console.log(chalk.green(content))))
+//         .catch(getError)
+// }
+
+getFiles('./arquivos/texto.md')
